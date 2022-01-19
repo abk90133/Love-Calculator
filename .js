d24354c7ff12052d3d9892b673c60f289bd65c5c
ABHISHEK SINGH <64076762+abk90133@users.noreply.github.com>
2022-01-19 15:34:41 +0530

    Update .js

--- .js
+++ .js
@@ -2,7 +2,9 @@ var name = prompt("Enter your name: ");
 var name1 = prompt("Enter your's Partner Name: ");
 
 var name = Math.random()*100 ;
-name = Math.floor(name);
+name = Math.floor(name) + 1;
+
+// This +1 will include the 100 to this.
 
 if (name > 0){
    alert("Your love percentage is: " + name + "%." + " You Really love each other alot!");
