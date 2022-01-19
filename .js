f60c3e8c0516ce1ed242ddb7ae41f95ad11f0814
ABHISHEK SINGH <64076762+abk90133@users.noreply.github.com>
2022-01-19 15:32:35 +0530

    Create .js

---
+++ .js
@@ -0,0 +1,11 @@
+var name = prompt("Enter your name: ");
+var name1 = prompt("Enter your's Partner Name: ");
+
+var name = Math.random()*100 ;
+name = Math.floor(name);
+
+if (name > 0){
+   alert("Your love percentage is: " + name + "%." + " You Really love each other alot!");
+} else {
+    alert("Your love percentage is: " + name + "%");
+}
